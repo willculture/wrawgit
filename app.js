5cfbb69eed18d45ce7692e8c5091f933efaf09f2
willculture <bboyer@qq.com>
2016-08-28 07:01:09 -0400

    加入注释

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var https = require('https');
 var murl = require('url');
-var cheerio = require('cheerio');
+var cheerio = require('cheerio'); //这个模块是用来讲Html字符串转换为DOM对象， 模仿了jQuery
 
+var token = undefined; //这里确定好一个token
 
-
-var token = undefined;
-var doto = false;
+/**
+ * 获取最新得git头， 来获取最新得提交代码
+ * @param path {String} 请求链接路径
+ * @param res {response} 响应对象
+ * @param callback {Function} 回调函数
+ */
 function getToken(path, res, callback) {
 	var project_path = path.slice(0, path.indexOf("/master"));
 	//首先要获取最新得tag
@@ -24,22 +28,24 @@ function getToken(path, res, callback) {
 			var $ = cheerio.load(result);
 			var href = $('a.js-permalink-shortcut').attr('href');
 			var href_arr = href.split('/');
-			token = href_arr[href_arr.length - 1];
-			doto = true;
-			console.log(doto);
+			token = href_arr[href_arr.length - 1]; 
 			callback(path, res);
 		});
 
 	});
 }
 
+/**
+ * 获取到得raw数据， 响应给客户端
+ * @param path {String} 请求路径
+ * @param res {response} 响应对象
+ */
 function doRawgit(path, res) {
 	// 
 	https.get({
 		hostname : 'raw.githubusercontent.com',
 		path : path.replace('master', token)
 	}, function(sres) {
-
 
 		res.statusCode = 200;
 		var content = "";
@@ -49,30 +55,29 @@ function doRawgit(path, res) {
 		});
 
 		sres.on("end", function() {
-			console.log("end the data")
+			//console.log("end the data")
 			res.end(content);
 		})
 	});
 
 }
 
+/**
+ * 创建服务
+ */
 var server = http.createServer(function(req, res) {
-
+	
 	var urlp = murl.parse(req.url);
 	var path = urlp.pathname;
 	var sc = urlp.search;
-	console.log(token)
-
-	if (!token) {
+ 
+	if (path.indexOf('master') > -1) {
 		getToken(path, res, doRawgit);
 	} else {
 		doRawgit(path, res);
 	}
-	
-
-	
-
 
 });
+
 var port = process.env.PORT || 5000;
 server.listen(port);
